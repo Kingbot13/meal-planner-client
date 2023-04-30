8041c73ea301663aa4fed595c4b399f9fb5138ae
Dylan King <dylan.wesley.king@gmail.com>
2023-04-29 19:59:05 -0500

    add value state and handleChange

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import { redirect } from "react-router-dom";
 
 export const Register = () => {
+
+    const [value, setValues] = useState({
+        username: "",
+        firstName: "",
+        lastName: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const handleChange = (e: any) => {
+        const valueCopy = {...value};
+        valueCopy[e.target.name] = e.target.value;
+        setValues(valueCopy);
+    }
     const guestSignIn = () => {
         const storage = localStorage;
         storage.getItem('guest') ? redirect('/') 
@@ -11,18 +26,18 @@ export const Register = () => {
     return (
         <div>
             <div>
-                <h1>Register or Log In</h1>
+                <h1>Register</h1>
                 <form>
                     <label htmlFor="username">Email:</label>
-                    <input type='text' name="username" id="username"></input>
+                    <input type='text' name="username" id="username" onChange={(e)=>handleChange} value={value.username}></input>
                     <label htmlFor="firstName">First Name:</label>
-                    <input type='text' name="firstName" id="firstName"></input>
+                    <input type='text' name="firstName" id="firstName" onChange={(e)=>handleChange} value={value.firstName}></input>
                     <label htmlFor="lastName">Last Name:</label>
-                    <input type='text' name="lastName" id="lastName"></input>
+                    <input type='text' name="lastName" id="lastName" onChange={(e)=>handleChange} value={value.lastName}></input>
                     <label htmlFor="password">Password:</label>
-                    <input type='password' name="password" id="password"></input>
+                    <input type='password' name="password" id="password" onChange={(e)=>handleChange} value={value.password}></input>
                     <label htmlFor="confirmPassword">Confirm Password:</label>
-                    <input type='password' name="confirmPassword" id="confirmPassword"></input> 
+                    <input type='password' name="confirmPassword" id="confirmPassword" onChange={(e)=>handleChange} value={value.confirmPassword}></input> 
                 </form>
                 <hr/>
                 <h2>Sign In as Guest</h2>
