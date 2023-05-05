a09ca22499e147f4315fc83402745db0da112fc9
Dylan King <dylan.wesley.king@gmail.com>
2023-05-04 19:38:28 -0500

    create RecipeCard component

---
+++ src/components/RecipeCard.tsx
@@ -0,0 +1,11 @@
+// this component could contain small key bits of information such as cooking time
+
+export const RecipeCard = (recipeName: string) => {
+    return (
+        <div>
+            <div>
+                <div>{recipeName}</div>
+            </div>
+        </div>
+    )
+}
