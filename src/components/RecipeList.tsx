af65e7cb6fdfce0252ae90a2911d9e00f506b7ec
Dylan King <dylan.wesley.king@gmail.com>
2023-05-08 21:46:27 -0500

    map guest recipes if guest mode is on

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,12 +1,17 @@
 import { RecipeCard } from "./RecipeCard";
 import { useGetUserRecipesQuery } from "../features/api/apiSlice";
+import { guestUtils } from "../app/guestUtils";
 
 export const RecipeList = () => {
+    const {isGuest} = guestUtils;
     const userId = localStorage.getItem('userId');
     const {data: recipes=[]} = useGetUserRecipesQuery(userId);
-    const content = recipes.map(recipe => {
-        return <RecipeCard recipeName={recipe.name} id={recipe._id} />
-    })
+    const content = isGuest? 
+        guestUtils.recipes.map(recipe => <RecipeCard recipeName={recipe.name} id={recipe.name} /> )
+        :
+        recipes.map(recipe => {
+            return <RecipeCard recipeName={recipe.name} id={recipe._id} />
+    });
     return(
         <ul>
             {content}
