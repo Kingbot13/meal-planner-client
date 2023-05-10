0a80f5b20f73cf121aa33e7bd57b67e189a03dc8
Dylan King <dylan.wesley.king@gmail.com>
2023-05-09 20:22:07 -0500

    lift state to Dashboard

--- src/components/RecipeForm.tsx
+++ src/components/RecipeForm.tsx
@@ -1,87 +1,19 @@
 import React, { useState } from "react";
 import { IngredientInputs } from "./IngredientInputs";
 import { RecipeInputs } from "./RecipeInputs";
-import { useAddRecipeMutation, useGetSingleRecipeQuery, useUpdateRecipeMutation } from "../features/api/apiSlice";
-import { guestUtils } from "../app/guestUtils";
 
-export const RecipeForm = () => {
-    const [ingredientValues, setIngredientValues] = useState([{name: "", measurement: ""}]);
-    const [recipeValues, setRecipeValues] = useState([{value: ""}]);
-    const [recipeName, setRecipeName] = useState("");
-    const [recipeInfo, setRecipeInfo] = useState({userId: '', recipeId: ''});
-
-    const [addRecipe] = useAddRecipeMutation();
-    const [updateRecipe] = useUpdateRecipeMutation();
-
-    const {data: recipe, isLoading} = useGetSingleRecipeQuery(recipeInfo);
-
-    const {isGuest, getGuestSingleRecipe} = guestUtils;
-
-    const handleIngredientChange = (i: number, e: any) => {
-        let valuesCopy = [...ingredientValues];
-        valuesCopy[i][e.target.name] = e.target.value;
-        setIngredientValues(valuesCopy);
-    };
-
-    const handleRecipeChange = (i: number, e: any) => {
-        let valuesCopy = [...recipeValues];
-        valuesCopy[i][e.target.name] = e.target.value;
-        setRecipeValues(valuesCopy);
-    }
-
-    const handleNameChange = (e: any) => {
-        setRecipeName(e.target.value);
-    }
-
-    const addIngredientFields = () => {
-        setIngredientValues([...ingredientValues, {name: "", measurement: ""}]);
-    };
-
-    const addRecipeFields = () => {
-        setRecipeValues([...recipeValues, {value:""}]);
-    }
-
-    const removeIngredientFields = (i: number) => {
-        const valuesCopy = [...ingredientValues];
-        valuesCopy.splice(i, 1);
-        setIngredientValues(valuesCopy);
-    }
-
-    const removeRecipeFields = (i: number) => {
-        const valuesCopy = [...recipeValues];
-        valuesCopy.splice(i, 1);
-        setRecipeValues(valuesCopy);
-    }
-    // TODO: add form validation
-    const submit = async () => {
-        if (isGuest) {
-            guestUtils.saveRecipe(recipeName, ingredientValues, recipeValues);
-        } else {
-            try{
-                const recipe = {name: recipeName, ingredients: [...ingredientValues], steps: [...recipeValues], userId: localStorage.getItem('userId')};
-                await addRecipe(recipe).unwrap();
-                setIngredientValues([{name: '', measurement: ''}]);
-                setRecipeName('');
-                setRecipeValues([{value: ''}]);
-            } catch(err) {
-                console.error(err);
-            }
-        }
-    }
-    // TODO: move function to where it can be reached by RecipeCard's update button. 
-    // state may need to be moved up to Dashboard
-    const toggleRecipeUpdate = (e) => {
-        if (isGuest) {
-            const recipe = getGuestSingleRecipe(e.target.dataId);
-            setIngredientValues(recipe.ingredients);
-            setRecipeName(recipe.name);
-            setRecipeValues(recipe.steps);
-        } else {
-            setIngredientValues(recipe.ingredients);
-            setRecipeName(recipe.name);
-            setRecipeValues(recipe.steps);
-        };
-    }
+export const RecipeForm = (
+    ingredientValues: object[], 
+    recipeValues: object[], 
+    handleNameChange: Function, 
+    handleIngredientChange: Function, 
+    handleRecipeChange: Function, 
+    removeIngredientFields: Function, 
+    addIngredientFields: Function,
+    removeRecipeFields: Function,
+    addRecipeFields: Function,
+    submit: Function 
+    ) => {
 
     return (
         <div>
@@ -104,7 +36,7 @@ export const RecipeForm = () => {
                     return <RecipeInputs key={index} number={index} value={item} onChange={handleRecipeChange} removeField={removeRecipeFields} addField={addRecipeFields} />
                 })}
             </form>
-            <button type="button" onClick={submit}>Create New Recipe</button>
+            <button type="button" onClick={()=>submit()}>Create New Recipe</button>
         </div>
     )
 }
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,9 +1,90 @@
 import { RecipeList } from "../components/RecipeList";
 import { RecipeForm } from "../components/RecipeForm";
 import { useState } from "react";
+import { useAddRecipeMutation, useGetSingleRecipeQuery, useUpdateRecipeMutation } from "../features/api/apiSlice";
+import { guestUtils } from "../app/guestUtils";
+
 
 export const Dashboard = () => {
     const [showForm, setShowForm] = useState(false);
+    const [ingredientValues, setIngredientValues] = useState([{name: "", measurement: ""}]);
+    const [recipeValues, setRecipeValues] = useState([{value: ""}]);
+    const [recipeName, setRecipeName] = useState("");
+    const [recipeInfo, setRecipeInfo] = useState({userId: '', recipeId: ''});
+
+    const [addRecipe] = useAddRecipeMutation();
+    const [updateRecipe] = useUpdateRecipeMutation();
+
+    const {data: recipe, isLoading} = useGetSingleRecipeQuery(recipeInfo);
+
+    const {isGuest, getGuestSingleRecipe} = guestUtils;
+
+    const handleIngredientChange = (i: number, e: any) => {
+        let valuesCopy = [...ingredientValues];
+        valuesCopy[i][e.target.name] = e.target.value;
+        setIngredientValues(valuesCopy);
+    };
+
+    const handleRecipeChange = (i: number, e: any) => {
+        let valuesCopy = [...recipeValues];
+        valuesCopy[i][e.target.name] = e.target.value;
+        setRecipeValues(valuesCopy);
+    }
+
+    const handleNameChange = (e: any) => {
+        setRecipeName(e.target.value);
+    }
+
+    const addIngredientFields = () => {
+        setIngredientValues([...ingredientValues, {name: "", measurement: ""}]);
+    };
+
+    const addRecipeFields = () => {
+        setRecipeValues([...recipeValues, {value:""}]);
+    }
+
+    const removeIngredientFields = (i: number) => {
+        const valuesCopy = [...ingredientValues];
+        valuesCopy.splice(i, 1);
+        setIngredientValues(valuesCopy);
+    }
+
+    const removeRecipeFields = (i: number) => {
+        const valuesCopy = [...recipeValues];
+        valuesCopy.splice(i, 1);
+        setRecipeValues(valuesCopy);
+    }
+    // TODO: add form validation
+    const submit = async () => {
+        if (isGuest) {
+            guestUtils.saveRecipe(recipeName, ingredientValues, recipeValues);
+        } else {
+            try{
+                const recipe = {name: recipeName, ingredients: [...ingredientValues], steps: [...recipeValues], userId: localStorage.getItem('userId')};
+                await addRecipe(recipe).unwrap();
+                setIngredientValues([{name: '', measurement: ''}]);
+                setRecipeName('');
+                setRecipeValues([{value: ''}]);
+            } catch(err) {
+                console.error(err);
+            }
+        }
+    }
+    // TODO: move function to where it can be reached by RecipeCard's update button. 
+    // state may need to be moved up to Dashboard
+    const toggleRecipeUpdate = (e) => {
+        if (isGuest) {
+            const recipe = getGuestSingleRecipe(e.target.dataId);
+            setIngredientValues(recipe.ingredients);
+            setRecipeName(recipe.name);
+            setRecipeValues(recipe.steps);
+        } else {
+            setIngredientValues(recipe.ingredients);
+            setRecipeName(recipe.name);
+            setRecipeValues(recipe.steps);
+        };
+    }
+
 
     const toggleForm = () => {
         setShowForm(!showForm ? true : false);
