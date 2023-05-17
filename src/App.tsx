0cd4ab3cdc2efa4f954202760578fd5e8f8560a5
Dylan King <dwkblue51@yahoo.com>
2023-05-17 10:46:36 -0500

    create Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import {Nav} from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Outlet />
     </div>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,8 +6,8 @@ export const Home = () => {
         <main>
             <h1>Welcome! Please sign in or register</h1>
             <div>
-                <Link to='/login'>Sign In</Link>
-                <Link to='/register'>Sign In</Link>
+                <Link to='/login'>Sign In or Guest Log in</Link>
+                <Link to='/register'>Register</Link>
             </div>
         </main>
     )
