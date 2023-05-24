936e97186692ad7442396d77a419a3700f58da3d
Dylan King <dwkblue51@yahoo.com>
2023-05-24 16:41:02 -0500

    add big blue circle decor

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Home = () => {
 
     return (
-        <main className="w-full h-full">
+        <main className="w-full h-full relative overflow-hidden">
             <div className="flex pl-28 pt-8">
                 <h1 className="text-5xl font-bold self-start">Meal Planner</h1> 
             </div>
@@ -11,7 +11,6 @@ export const Home = () => {
                 <h2 className="text-2xl flex w-[46rem] items-center justify-center">
                 Never think about what's for dinner again! <br/>
                 Get started with a <span className="text-warmth font-bold contents"> free account</span> or sign in
-                
                 </h2>
                 <p className="max-w-2xl text-xl">
                 It's time to stop spending your days wondering what you should eat for dinner. 
@@ -21,6 +20,9 @@ export const Home = () => {
                 <Link to='/login'>Sign In or Guest Log in</Link>
                 <Link to='/register'>Register</Link>
             </div>
+            <div className="bg-noon-sky/75 w-[46.5rem] h-[46.5rem] rounded-full absolute left-[23vw] top-[43vh] -z-10"></div>
+            {/* decorative divs */}
+            <div></div>
         </main>
     )
 }
