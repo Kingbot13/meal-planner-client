a449bc69e012cf28b85ce9a1fd8740ed503d11b6
Dylan King <dwkblue51@yahoo.com>
2023-05-25 09:02:18 -0500

    style icon in Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import {Bars3Icon} from '@heroicons/react/24/outline'
 export const Nav = () => {
     return (
         <nav className="rounded-full absolute left-[89vw] -top-[15vh] w-80 h-80 bg-bubble-gum overflow-hidden">
-            <Bars3Icon className=''/>
+            <Bars3Icon className='absolute top-40 h-16 w-16 left-16 stroke-[#fff]'/>
 
         </nav>
     )
