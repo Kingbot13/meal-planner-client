fc090fb88f68341f8edae169de2e965ba9853a3d
Dylan King <dwkblue51@yahoo.com>
2023-05-25 09:43:14 -0500

    minor style adjustments

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -10,19 +10,19 @@ export const Home = () => {
             <div className="flex w-full flex-col items-center h-full justify-between py-16">
                 <h2 className="text-2xl flex w-[46rem] items-center justify-center justify-self-start mt-0">
                 Never think about what's for dinner again! <br/>
-                Get started with a <span className="text-warmth font-bold contents"> free account</span> or sign in
+                Get started with a <Link to='/register' className="text-warmth font-bold contents"> free account</Link> or <Link className="contents" to='/login'>sign in</Link> 
                 </h2>
-                <p className="max-w-lg leading-10 text-xl mb-20">
+                <div className="bg-noon-sky/75 w-[46.5rem] h-[46.5rem] rounded-full relative top-32 -z-10 flex items-center justify-center">
+
+                <p className="max-w-lg leading-10 text-xl mb-20 indent-8">
                 It's time to stop spending your days wondering what you should eat for dinner. 
                 With Meal Planner, all you have to do is enter in your own favorite recipes and 
                 <span className="font-bold italic"> voila! </span> Your recipes are automagically mixed up and scheduled on a specific day!
                 </p>
-                {/* <Link to='/login'>Sign In or Guest Log in</Link>
-                <Link to='/register'>Register</Link> */}
+                </div>
             </div>
-            <div className="bg-noon-sky/75 w-[46.5rem] h-[46.5rem] rounded-full absolute left-[23vw] top-[43vh] -z-10"></div>
-            <div className="bg-sea-turtle w-[51rem] h-[51rem] rounded-full absolute -z-20 -left-[12vw] top-[64vh]"></div>
             {/* decorative divs */}
+            <div className="bg-sea-turtle w-[51rem] h-[51rem] rounded-full absolute -z-20 -left-[4vw] top-[64vh]"></div>
             <div></div>
         </main>
     )
