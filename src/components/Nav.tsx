193de56fc8acee0917c2e3a4ffcb6a142f3aaf46
Dylan King <dylan.wesley.king@gmail.com>
2023-05-24 22:22:51 -0500

    add icon to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
-
+import {Bars3Icon} from '@heroicons/react/24/outline'
 
 export const Nav = () => {
     return (
         <nav className="rounded-full absolute left-[89vw] -top-[15vh] w-80 h-80 bg-bubble-gum overflow-hidden">
-            
+            <Bars3Icon className=''/>
+
         </nav>
     )
 }
+
