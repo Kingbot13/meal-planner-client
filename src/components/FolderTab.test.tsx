8df28ab7c6399e449af44112e0ee2c6cbbfa8030
Dylan King <dwkblue51@yahoo.com>
2023-06-15 10:33:40 -0500

    more tests

--- src/components/FolderTab.test.tsx
+++ src/components/FolderTab.test.tsx
@@ -19,5 +19,19 @@ describe("FolderTab", () => {
         await userE.click(tab);
 
         expect(folder).toHaveClass('folder-active');
+    });
+
+    it("removes class 'folder-active' after different p element clicked", async() => {
+        renderWithProviders(<Dashboard />);
+        const tab = screen.getByText(/week/i);
+        const folder = screen.getByTestId(/ft-week/i);
+        const tab2 = screen.getByText(/today/i);
+        const folder2 = screen.getByTestId(/ft-today/i);
+
+        await userE.click(tab);
+        await userE.click(tab2);
+
+        expect(folder).not.toHaveClass('folder-active');
+        expect(folder2).toHaveClass('folder-active');
     })
 })
