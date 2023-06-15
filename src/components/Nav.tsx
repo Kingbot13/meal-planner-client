f26eef395c088fcd20d7e14593a2be0140712985
Dylan King <dwkblue51@yahoo.com>
2023-06-15 12:02:23 -0500

    style nav with rem instead of vw and vh

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import {Bars3Icon} from '@heroicons/react/24/outline'
 
 export const Nav = () => {
     return (
-        <nav className="rounded-full absolute left-[89vw] -top-[15vh] w-80 h-80 bg-bubble-gum overflow-hidden">
+        <nav className="rounded-full absolute -right-40 -top-40 w-80 h-80 bg-bubble-gum overflow-hidden">
             <Bars3Icon className='absolute top-44 h-16 w-16 left-16 stroke-[#fff]'/>
 
         </nav>
