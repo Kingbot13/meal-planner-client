e6cde925c512d13d4dc6c6d7be49c07be1c9e36e
Dylan King <dwkblue51@yahoo.com>
2023-06-20 11:56:57 -0500

    add animation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,25 +4,20 @@ import React, { useEffect, useRef } from 'react'
 export const Nav = () => {
 
     const svgRef = useRef<SVGSVGElement>(null);
-
-    // useEffect(() => {
-    //     if (svgRef.current) {
-    //         svgRef.current.focus();
-    //     }
-    // },[]);
+    const divRef = useRef<HTMLDivElement>(null);
 
     const handleClick = (e: React.MouseEvent) => {
-        if (svgRef.current) {
-            svgRef.current.focus();
+        if (divRef.current) {
+            if (!divRef.current.classList.contains('hamburger-active')) divRef.current.classList.add('hamburger-active');
+            else divRef.current.classList.remove('hamburger-active');
         }
 
-        console.log(e.target);
     }
     return (
         <nav className="rounded-full absolute -right-40 -top-40 w-80 h-80 bg-bubble-gum overflow-hidden ">
             {/* <Bars3Icon className='absolute top-44 h-16 z-10 w-16 left-16 stroke-[#fff] transition-transform target:-rotate-90 '/> */}
-            <div tabIndex={0} onClick={(e) => handleClick(e)} className='absolute hamburger top-44 left-16'>
-                <svg ref={svgRef} onClick={(e) => handleClick(e)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className=" h-16 w-16 stroke-[#fff] ">
+            <div ref={divRef} onClick={(e) => handleClick(e)} tabIndex={0} className='absolute  hamburger top-44 left-16'>
+                <svg ref={svgRef} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className=" h-16 transition-transform focus:-rotate-90 w-16 stroke-[#fff] ">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
             </div>
